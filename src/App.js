57cc4b366e453a5349d54efd639a106dc934ad6b
Gaillard <nicolas.gaillard@edu.ece.fr>
2022-11-09 23:01:13 +0100

    INITIALISATION

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import abi from './contracts/ABI.json';
+import { ethers } from 'ethers';
+
+const contractAddress = "0xFfD0E378cE7220F27e12373C39D324F5961d3e4b";
+
+function App() {
+
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  const checkWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have Metamask installed!");
+      return;
+    } else {
+      console.log("Wallet exists! We're ready to go!")
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account: ", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  }
+
+  const connectWalletHandler = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      alert("Please install Metamask!");
+    }
+
+    try {
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      console.log("Found an account! Address: ", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const mintNftHandler = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const nftContract = new ethers.Contract(contractAddress, abi, signer);
+
+        console.log("Initialize payment");
+        let nftTxn = await nftContract.safeMint();
+
+        console.log("Mining... please wait");
+        await nftTxn.wait();
+
+        console.log(`Mined, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`);
+
+      } else {
+        console.log("Ethereum object does not exist");
+      }
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const connectWalletButton = () => {
+    return (
+      <button onClick={connectWalletHandler} className='cta-button connect-wallet-button'>
+        Connect Wallet
+      </button>
+    )
+  }
+
+  const mintNftButton = () => {
+    return (
+      <button onClick={mintNftHandler} className='cta-button mint-nft-button'>
+        Mint NFT
+      </button>
+    )
+  }
+
+  useEffect(() => {
+    checkWalletIsConnected();
+  }, [])
+
+  return (
+    <div className='main-app'>
+      <h1>CRAZY DOG CD7 MINT NOW</h1>
+      <h2>TP ECE </h2>
+      <div>
+        {currentAccount ? mintNftButton() : connectWalletButton()}
+      </div>
+      <img alt='img' src="https://i.seadn.io/gae/Sp5hBj8O5ngsptAHkOeXb2sVG5mCeO6LIBR4_obqgXHgAiwHw8IcATHQHGmGMu7a3Ni4tGMlwjzrw6OYWmq8hkvarFGuvoQf0_4kyQ?auto=format&w=1000"></img>
+    </div >
+  )
+}
+
+export default App;
